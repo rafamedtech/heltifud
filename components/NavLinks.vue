f268cd2a3ccd5142f999a1c3a5a8c8929ba1deb4
Rafael <rafamed.tech@gmail.com>
2023-10-25 11:58:46 -0700

    link active and hover styles matched

--- components/NavLinks.vue
+++ components/NavLinks.vue
@@ -3,8 +3,8 @@
 <template>
   <li>
     <NuxtLink
-      active-class="underline"
-      class="text-base lg:text-sm"
+      active-class="underline text-white"
+      class="text-base lg:text-sm hover:underline hover:text-white hover:bg-transparent"
       to="https://heltifud.com"
       rel="noreferrer"
       >Inicio</NuxtLink
@@ -12,22 +12,26 @@
   </li>
   <li>
     <NuxtLink
-      active-class="underline"
-      class="text-base lg:text-sm"
+      active-class="underline text-white"
+      class="text-base lg:text-sm hover:underline hover:text-white hover:bg-transparent"
       to="https://heltifud.com/collections/planes"
       rel="noreferrer"
       >Nuestros planes</NuxtLink
     >
   </li>
   <li>
-    <NuxtLink active-class="underline" class="text-base lg:text-sm" to="/" rel="noreferrer"
+    <NuxtLink
+      active-class="underline text-white"
+      class="text-base lg:text-sm hover:underline hover:text-white hover:bg-transparent"
+      to="/"
+      rel="noreferrer"
       >Menú</NuxtLink
     >
   </li>
   <li>
     <NuxtLink
-      active-class="underline"
-      class="text-base lg:text-sm"
+      active-class="underline text-white"
+      class="text-base lg:text-sm hover:underline hover:text-white hover:bg-transparent"
       to="https://heltifud.com/pages/contact"
       rel="noreferrer"
       >Contacto</NuxtLink
