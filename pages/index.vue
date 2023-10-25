9848d1d7dd8a1e27683770ab37a04b4561ce49bd
Rafael <rafamed.tech@gmail.com>
2023-10-25 09:21:35 -0700

    collapse removed on desktop removed heading sticky

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@ useHead({
       name: 'description',
       content: 'Servicio de Meal preps saludables, caseros y flexibles a domicilio.',
     },
-    // Facbebook Meta tags
+    // Facebook Meta tags
     {
       property: 'og:type',
       content: 'website',
@@ -49,7 +49,7 @@ useHead({
       <section class="mt-8">
         <section class="grid gap-4 lg:grid-cols-5">
           <div v-for="day in menu.days">
-            <div class="collapse rounded-none lg:max-w-lg bg-white">
+            <div class="collapse lg:hidden rounded-none lg:max-w-lg bg-white">
               <input type="radio" name="my-accordion-1" />
               <div class="collapse-title text-xl font-medium">
                 <h2 class="card-title text-base-100 text-3xl">{{ day.name }}</h2>
@@ -91,6 +91,54 @@ useHead({
                       <p class="w-fit text-end">{{ day.totalCalories }} Cal</p>
                     </section>
                   </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="hidden lg:block max-w-lg bg-white min-h-[30rem] max-h-[30rem]">
+              <!-- <input type="radio" name="my-accordion-1" /> -->
+              <div class="collapse-title text-xl font-medium">
+                <h2 class="card-title text-base-100 text-3xl">{{ day.name }}</h2>
+              </div>
+              <div class="lg:text-xs p-4 h-full">
+                <div
+                  class="justify-between flex flex-col rounded-none h-full bg-white text-base-100"
+                >
+                  <div class="">
+                    <!-- <h2 class="card-title text-primary mb-6 text-3xl">{{ day.name }}</h2> -->
+                    <section>
+                      <ul class="flex flex-col gap-4">
+                        <li v-for="course in day.courses">
+                          <h3 class="font-bold text-xl text-primary">{{ course.name }}</h3>
+                          <ul>
+                            <li v-for="meal in course.meals">
+                              <ul class="flex flex-col gap-2">
+                                <li class="flex gap-2 justify-between w-full">
+                                  <span class="w-1/2 block"> {{ meal.main.name }}</span>
+                                  <span class="w-1/2 block text-end"
+                                    >{{ meal.main.calories }} Cal</span
+                                  >
+                                </li>
+                                <li v-if="meal.side" class="flex gap-2 justify-between w-full">
+                                  <span class="w-1/2 block"> {{ meal.side?.name }}</span>
+                                  <span class="w-1/2 block text-end"
+                                    >{{ meal.side?.calories }} Cal</span
+                                  >
+                                </li>
+                                <!-- <li v-if="meal.side2">
+                            <span>{{ meal.side2?.name }}</span>
+                          </li> -->
+                              </ul>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </section>
+                  </div>
+                  <section class="mt-8 flex justify-end gap-2 items-center w-full">
+                    <span class="font-bold">Total:</span>
+                    <p class="w-fit text-end">{{ day.totalCalories }} Cal</p>
+                  </section>
                 </div>
               </div>
             </div>
