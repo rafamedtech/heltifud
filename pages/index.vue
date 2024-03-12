411cf5caca70e85fd02adab5a2d1ac093cff4c39
Rafael <rafamed.tech@gmail.com>
2024-03-12 10:14:45 -0700

    Refactor code to improve performance and readability

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,80 @@
 <script setup lang="ts">
-import { menu } from '@/utils/menu';
+const menuBackup1 = [
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1710262707/heltifud/18%20Mar%20-%2022%20Mar/Lunes_ucp302.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1710262708/heltifud/18%20Mar%20-%2022%20Mar/Martes_gk6jpk.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1710262709/heltifud/18%20Mar%20-%2022%20Mar/Miercoles_gsmbac.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1710262706/heltifud/18%20Mar%20-%2022%20Mar/Jueves_gf49ms.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1710262710/heltifud/18%20Mar%20-%2022%20Mar/Viernes_mj9my6.png',
+];
 
-interface Day {
-  name: string;
-  courses: Course[];
-}
+const menuBackup2 = [
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1710262907/heltifud/25%20Mar%20-%2029%20Mar/Lunes_ba59rx.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1710262904/heltifud/25%20Mar%20-%2029%20Mar/Martes_qxar3n.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1710262908/heltifud/25%20Mar%20-%2029%20Mar/Miercoles_n9hu4n.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1710262903/heltifud/25%20Mar%20-%2029%20Mar/Jueves_oel5v2.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1710262905/heltifud/25%20Mar%20-%2029%20Mar/Viernes_sgihsf.png',
+];
 
-interface Course {
-  name: string;
-  meals: Meal[];
-}
+const days = [
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1709852532/heltifud/11%20Mar%20-%2015%20Mar/Lunes_tvvwvv.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1709852532/heltifud/11%20Mar%20-%2015%20Mar/Martes_hxq409.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1709852532/heltifud/11%20Mar%20-%2015%20Mar/Miercoles_ayyfgo.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1709852532/heltifud/11%20Mar%20-%2015%20Mar/Jueves_swg7r0.png',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1709852533/heltifud/11%20Mar%20-%2015%20Mar/Viernes_sfozto.png',
+];
 
-interface Meal {
-  id: number;
-  name: string;
-  calories: number;
+const menu = computed(() => {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+  const currentDay = currentDate.getDate();
+
+  if (currentMonth === 2) {
+    // March is month 2 in JavaScript Date
+    if (currentDay >= 13 && currentDay <= 19) {
+      return menuBackup1;
+    } else if (currentDay >= 20 && currentDay <= 26) {
+      return menuBackup2;
+    }
+  }
+
+  return days;
+});
+
+const menuDate = computed(() => {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+  const currentDay = currentDate.getDate();
+
+  if (currentMonth === 2) {
+    // March is month 2 in JavaScript Date
+    if (currentDay >= 13 && currentDay <= 19) {
+      return '18 Marzo - 22 Marzo';
+    } else if (currentDay >= 20 && currentDay <= 26) {
+      return '25 Marzo - 29 Marzo';
+    }
+  }
+
+  return '11 Marzo - 15 Marzo';
+});
+
+function indexName(index: number) {
+  if (index === 1) return 'Lunes';
+  if (index === 2) return 'Martes';
+  if (index === 3) return 'Miércoles';
+  if (index === 4) return 'Jueves';
+  if (index === 5) return 'Viernes';
 }
 
 const isLoading = ref(true);
 
 onMounted(() => (isLoading.value = false));
 
-const sumCaloriesForDay = (day: Day) => {
-  return day.courses.reduce((totalCalories: number, course: Course) => {
-    return (
-      totalCalories + course.meals.reduce((total: number, meal: Meal) => total + meal.calories, 0)
-    );
-  }, 0);
-};
-
 useHead({
-  title: 'Heltifud Meal preps',
+  title: 'Menú de la semana | Heltifud Meal preps',
   meta: [
     {
       name: 'description',
-      content: 'Servicio de Meal preps saludables, caseros y flexibles a domicilio.',
+      content: 'Este es nuestro menú para la próxima semana.',
     },
     // Facebook Meta tags
     {
@@ -51,7 +91,7 @@ useHead({
     },
     {
       property: 'og:description',
-      content: 'Servicio de Meal preps saludables, caseros y flexibles a domicilio.',
+      content: 'Este es nuestro menú para la próxima semana.',
     },
     {
       property: 'og:image',
@@ -65,81 +105,44 @@ useHead({
 <template>
   <MainSection :loading="isLoading">
     <template #heading>
-      <AppHeading title="Menú de la próxima semana" :description="menu.dates" />
+      <AppHeading title="Menú de la próxima semana" :description="menuDate" />
     </template>
 
     <!-- Page content -->
     <template #content>
-      <section class="mt-8">
-        <section class="grid gap-2 lg:grid-cols-5">
-          <div v-for="day in menu.days">
-            <div class="collapse lg:hidden rounded-none lg:max-w-lg bg-white">
-              <input type="radio" name="my-accordion-1" />
-              <div class="collapse-title text-xl font-medium">
-                <h2 class="card-title text-base-100 text-3xl">{{ day.name }}</h2>
-              </div>
-              <div class="collapse-content lg:text-xs">
-                <div class="justify-between rounded-none h-full bg-white text-base-100">
-                  <section>
-                    <ul class="flex flex-col gap-4">
-                      <li v-for="course in day.courses">
-                        <h3 class="font-bold text-xl text-primary">{{ course.name }}</h3>
-                        <ul class="mt-2">
-                          <li v-for="meal in course.meals">
-                            <ul class="flex flex-col gap-2">
-                              <li class="flex gap-2 justify-between w-full">
-                                <Icon name="icon-park-outline:arrow-right" class="text-primary" />
-                                <span class="w-1/2 block"> {{ meal.name }}</span>
-                                <span class="w-1/2 block text-end">{{ meal.calories }} Cal</span>
-                              </li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </section>
-                  <section class="mt-8 flex gap-2 items-center justify-end w-full h-16">
-                    <span class="font-bold text-primary">Total:</span>
-                    <p class="w-fit text-end font-bold">{{ sumCaloriesForDay(day) }} Cal</p>
-                  </section>
-                </div>
-              </div>
-            </div>
+      <section class="lg:hidden flex justify-center">
+        <UButton label="Ordenar" size="lg" to="https://wa.me/c/5216648161284">
+          <template #trailing><Icon name="heroicons:rocket-launch" size="24" /></template>
+        </UButton>
+      </section>
+      <section class="mt-8 pb-8">
+        <UCarousel
+          :items="menu"
+          :ui="{
+            item: 'basis-full',
+            container: 'rounded-lg',
+            indicators: {
+              wrapper: 'relative bottom-0 mt-4 gap-2 max-w-full',
+            },
+          }"
+          indicators
+          class="min-w-full max-w-screen-md mx-auto"
+        >
+          <template #default="{ item }">
+            <img :src="item" class="w-full rounded-xl" draggable="false" />
+          </template>
 
-            <div class="hidden lg:block max-w-lg bg-white min-h-[35rem] max-h-[30rem]">
-              <div class="collapse-title text-xl font-medium h-16">
-                <h2 class="card-title text-base-100 text-3xl">{{ day.name }}</h2>
-              </div>
-
-              <div
-                class="justify-between flex flex-col rounded-none min-h-[31rem] h-[31rem] max-h-[31rem] bg-white text-base-100 lg:text-xs p-4"
-              >
-                <ul class="flex flex-col gap-4 flex-1 items-stretch">
-                  <li v-for="course in day.courses" class="h-full min-h-max">
-                    <h3 class="font-bold text-xl text-primary">{{ course.name }}</h3>
-
-                    <ul class="flex gap-1 justify-between w-full flex-col mt-2">
-                      <li v-for="meal in course.meals">
-                        <article class="flex gap-1">
-                          <Icon name="icon-park-outline:arrow-right" class="text-primary" />
-                          <section class="flex justify-between w-full">
-                            <span class="w-2/3 block"> {{ meal.name }}</span>
-                            <span class="w-1/3 block text-end">{{ meal.calories }} Cal</span>
-                          </section>
-                        </article>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-
-                <section class="mt-8 flex gap-2 items-center justify-end w-full h-16">
-                  <span class="font-bold text-primary">Total:</span>
-                  <p class="w-fit text-end font-bold">{{ sumCaloriesForDay(day) }} Cal</p>
-                </section>
-              </div>
-            </div>
-          </div>
-        </section>
+          <template #indicator="{ onClick, index, active }">
+            <UButton
+              :label="indexName(index)"
+              :variant="active ? 'solid' : 'outline'"
+              size="sm"
+              class="rounded-xl min-w-6 justify-center"
+              :color="active ? 'primary' : 'gray'"
+              @click="onClick(index)"
+            />
+          </template>
+        </UCarousel>
       </section>
     </template>
   </MainSection>
