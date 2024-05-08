b018f25e0d602b1b21580035b7adfa2800ed841b
Rafael <rafamed.tech@gmail.com>
2024-05-08 09:11:21 -0700

    Update text color and font size in Course.vue

--- components/Course.vue
+++ components/Course.vue
@@ -8,17 +8,17 @@ const { item } = defineProps<{
 <template>
   <section class="pt-4 w-full h-full">
     <h4 class="text-primary-500 text-xl text-center">{{ label }}</h4>
-    <section class="grid grid-rows-3 pt-2 text-white text-lg">
-      <div class="flex gap-2 justify-center items-center h-1/3">
-        <span>{{ item.main.name }}</span>
+    <section class="grid grid-rows-3 pt-4 text-white text-lg lg:text-base">
+      <div class="flex gap-2 justify-between items-center h-1/3">
+        <span class="">{{ item.main.name }}</span>
         <span>{{ Number(item.main.calories).toFixed(0) }}Cal</span>
       </div>
-      <div class="flex gap-2 justify-center items-center h-1/3" v-if="item.side1.name">
-        <span>{{ item.side1.name }}</span>
+      <div class="flex gap-2 justify-between items-center h-1/3" v-if="item.side1.name">
+        <span class="">{{ item.side1.name }}</span>
         <span>{{ Number(item.side1.calories).toFixed(0) }}Cal</span>
       </div>
-      <div class="flex gap-2 justify-center items-center h-1/3" v-if="item.side2.name">
-        <span>{{ item.side2.name }}</span>
+      <div class="flex gap-2 justify-between items-center h-1/3" v-if="item.side2.name">
+        <span class="">{{ item.side2.name }}</span>
         <span>{{ Number(item.side2.calories).toFixed(0) }}Cal</span>
       </div>
     </section>
